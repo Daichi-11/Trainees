33db59f5d345e625b998621bf576c1201e19f055
Daichi-11 <dai77yama@gmail.com>
2023-12-11 01:07:49 +0900

    fix: login and signup

--- users/forms.py
+++ users/forms.py
@@ -3,61 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-	email = forms.EmailField(
-		label='',
-		max_length=254,
-		widget=forms.EmailInput(
-			attrs={
-				"placeholder": "Email",
-				"class": "form-control"
-			}
-		)
-	)
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=200)
 
-	username = forms.CharField(
-		label='',
-		max_length=30,
-		min_length=5,
-		required=True,
-		widget=forms.TextInput(
-			attrs={
-				"placeholder": "Username",
-				"class": "form-control"
-			}
-		)
-	)
-
-	password1 = forms.CharField(
-		label='',
-		max_length=30,
-		min_length=8,
-		required=True,
-		widget=forms.PasswordInput(
-			attrs={
-				"placeholder": "Password",
-				"class": "form-control"
-			}
-		)
-	)
-
-	password2 = forms.CharField(
-		label='',
-		max_length=30,
-		min_length=8,
-		required=True,
-		widget=forms.PasswordInput(
-			attrs={
-				"placeholder": "Confirm Password",
-				"class": "form-control"
-			}
-		)
-	)
-	
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-		
-class Meta:
-	model = User
-	fields = ('username', 'email', 'password1', 'password2')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
--- users/views.py
+++ users/views.py
@@ -1,30 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login,logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+
+
 
 def user_signup(request):
-    if request.user.is_authenticated:
-        return redirect('home')  
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Correct the errors below')
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
     else:
         form = SignUpForm()
-
     return render(request, 'users/signup.html', {'form': form})
+
 
 def user_login(request):
     if request.method == "POST":
@@ -35,8 +28,6 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
-        else:
-            messages.error(request, 'Correct the errors below')
     else:
         form = AuthenticationForm()
 
