c38db52dbced5fcd91cab6da0f993b0300cddf24
Daichi-11 <dai77yama@gmail.com>
2023-12-21 12:07:29 +0900

    add: profile delete and edit function

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import SignUpForm
+from .forms import SignUpForm,  UserProfileForm
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.decorators import login_required
 
 
 def user_signup(request):
@@ -40,3 +40,28 @@ def user_logout(request):
 
 def user_profile(request):
     return render(request, 'users/profile.html')
+
+
+# views.py
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+            return redirect('users:profile')
+    else:
+        form = UserProfileForm(instance=request.user.userprofile)
+    
+    return render(request, 'users/edit_profile.html', {'form': form})
+
+
+# views.py
+@login_required
+def delete_profile(request):
+    if request.method == 'POST':
+        request.user.userprofile.delete()
+        logout(request)
+        return redirect('users:login')
+
+    return render(request, 'users/delete_profile.html')
