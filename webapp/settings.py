4b3d9268f605769e1371423117484cfc3369a4b3
Daichi-11 <dai77yama@gmail.com>
2023-12-26 10:17:09 +0900

    create postgresql

--- webapp/settings.py
+++ webapp/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'traineesdb',         
+        'USER': 'postgres',
+        'PASSWORD': 'aTJDvCAkWunxmkl',
+        'HOST': 'traineesdb.internal', 
+        'PORT': '5432',              
     }
 }
 
